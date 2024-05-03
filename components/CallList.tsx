b4b2beacde175e63d1c966a0219594c92108689b
dharnnyel <ubahchuks91@gmail.com>
2024-05-03 11:45:14 +0100

    fix recording bug

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -2,13 +2,15 @@
 
 'use client';
 
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
 import { useGetCalls } from '@/hooks/useGetCalls';
 import {
 	Call,
 	CallRecording,
 } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+
 import MeetingCard from './MeetingCard';
 import Loader from './Loader';
 
@@ -19,14 +21,14 @@ type CallListProps = {
 };
 
 const CallList: React.FC<CallListProps> = ({ type }) => {
+	const router = useRouter();
+
 	const {
 		endedCalls,
 		callRecordings,
 		upcomingCalls,
 		isLoading,
 	} = useGetCalls();
-
-	const router = useRouter();
 
 	const [recordings, setRecordings] = useState<
 		CallRecording[]
@@ -50,28 +52,39 @@ const CallList: React.FC<CallListProps> = ({ type }) => {
 		switch (type) {
 			case 'ended':
 				return 'No Previous Calls';
-				break;
-			case 'recordings':
-				return 'No Recordings';
 			case 'upcoming':
 				return 'No Upcoming Calls';
+			case 'recordings':
+				return 'No Recordings';
 			default:
 				return '';
 		}
 	};
 
-  useEffect(() => {
-    const fetchRecordings = async () => {
-      
-    };
+	useEffect(() => {
+		const fetchRecordings = async () => {
+			const callData = await Promise.all(
+				callRecordings?.map(meeting =>
+					meeting.queryRecordings()
+				) ?? []
+			);
 
-    if(type === 'recordings') fetchRecordings()
-  }, [type]);
+			const recordings = callData
+				.filter(call => call.recordings.length > 0)
+				.flatMap(call => call.recordings);
+
+			setRecordings(recordings);
+		};
+
+		if (type === 'recordings') {
+			fetchRecordings();
+		}
+	}, [type, callRecordings]);
+
+	if (isLoading) return <Loader />;
 
 	const calls = getCalls();
 	const noCallsMessage = getNoCallsMessage();
-
-	if (isLoading) return <Loader />;
 
 	return (
 		<div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
@@ -89,14 +102,23 @@ const CallList: React.FC<CallListProps> = ({ type }) => {
 						title={
 							(
 								meeting as Call
-							).state.custom.description.substring(0, 23) ||
+							).state?.custom.description.substring(
+								0,
+								23
+							) ||
+							(meeting as CallRecording).filename.substring(
+								0,
+								20
+							) ||
 							'No description available'
 						}
 						date={
 							(
 								meeting as Call
-							).state.startsAt?.toLocaleString() ||
-							meeting.start_time.toLocaleString()
+							).state?.startsAt?.toLocaleString() ||
+							(
+								meeting as CallRecording
+							).start_time?.toLocaleString()
 						}
 						isPreviousMeeting={type === 'ended'}
 						buttonIcon1={
@@ -106,17 +128,19 @@ const CallList: React.FC<CallListProps> = ({ type }) => {
 						}
 						handleClick={
 							type === 'recordings'
-								? () => {
-										router.push(`${meeting.url}`);
-								  }
-								: () => {
-										router.push(`/meeting/${meeting.id}`);
-								  }
+								? () =>
+										router.push(
+											`${(meeting as CallRecording).url}`
+										)
+								: () =>
+										router.push(`/meeting/${(meeting as Call).id}`)
 						}
 						link={
 							type === 'recordings'
-								? meeting.url
-								: `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+								? (meeting as CallRecording).url
+								: `${
+										process.env.NEXT_PUBLIC_BASE_URL
+								  }/meeting/${(meeting as Call).id}`
 						}
 						buttonText={
 							type === 'recordings' ? 'Play' : 'Start'
